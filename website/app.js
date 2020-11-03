6b7a0db381f3c845ec5762423d14cbd4872c13a3
karimanhossam <kariman.badr@student.guc.edu.eg>
2020-11-03 23:54:50 +0200

    Added an async function that makes a post request to add the data

--- website/app.js
+++ website/app.js
@@ -1,3 +1,5 @@
+const { Console } = require("console");
+
 /* Global Variables */
 let baseURL = "api.openweathermap.org/data/2.5/weather?zip=";
 let apiKey = "&appid=120b23c2ee4e14313b56a5f17616d19d";
@@ -6,12 +8,33 @@ let apiKey = "&appid=120b23c2ee4e14313b56a5f17616d19d";
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
+// Async get
 const getData = async (baseURL, zipCode, key) => {
   const res = await fetch(baseURL + zipCode + key);
   try {
     const data = await res.json();
     console.log(data);
     return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+//Async post
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+  try {
+    const newData = await response.json();
+    Console.log(newData);
+    return newData;
   } catch (error) {
     console.log("error", error);
   }
