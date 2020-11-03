8e9d8fb4e35247154b939c51cccb929836d53506
karimanhossam <kariman.badr@student.guc.edu.eg>
2020-11-03 23:38:39 +0200

    Added an async function that uses fetch to make a get request to the weather API

--- website/app.js
+++ website/app.js
@@ -1,5 +1,18 @@
 /* Global Variables */
+let baseURL = "api.openweathermap.org/data/2.5/weather?zip=";
+let apiKey = "&appid=120b23c2ee4e14313b56a5f17616d19d";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+const getData = async (baseURL, zipCode, key) => {
+  const res = await fetch(baseURL + zipCode + key);
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
